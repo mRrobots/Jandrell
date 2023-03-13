3e7180a8d67de28b3fcce8b32866fc056c4e6990
mRrobots <mosismdlalose@gmail.com>
2023-03-13 21:06:01 +0200

    feature:intro card

--- src/app/components/Main/Main.js
+++ src/app/components/Main/Main.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import DDCard from './DDCard'
-import HomeIntro from './HomeIntro'
-import MainStyle from "./Main.module.css"
+import React from "react";
+import DDCard from "./DDCard";
+import HomeIntro from "./HomeIntro";
+import MainStyle from "./Main.module.css";
 
 export default function Main() {
-
-  const mainStyle = {
-    
-  }
+  const mainStyle = {};
   return (
     <div className={MainStyle.main}>
-      <div className='home-intro'>
-        <HomeIntro/>
+      <div className="home-intro">
+        <HomeIntro />
       </div>
-      <div className='diary-date-card'>
-        <DDCard/>
+      <div className="diary-date-card">
+        <DDCard />
       </div>
     </div>
-  )
+  );
 }
--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -3,14 +3,12 @@ import Main from "../components/Main/Main";
 import HomeBar from "../components/NavBar/HomeBar";
 import Nav from "../components/NavBar/Nav";
 
-export default function Home()
-{
-    return(
-        <div>
-            <Nav/>
-            <Main/>
-            <Footer/>
-            {/* <HomeBar/> */}
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
