73add59a412d54e85064e0ac6e530284d7136ebe
mRrobots <mosismdlalose@gmail.com>
2023-03-21 11:34:43 +0200

    feature:Contact page No style

--- src/app/pages/Contacts/Contacts.js
+++ src/app/pages/Contacts/Contacts.js
@@ -1,5 +1,31 @@
 import React from "react";
+import style from "./Contacts.module.css";
 
 export default function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div className={style.main}>
+      <div>
+        <h1>Contact Us</h1>
+        <h2>Address</h2>
+        <div className={style.address}>
+          G.S. College of Commerce, Jamanalal Bajaj Marg, Civil Lines, Wardha,
+          Maharashtra – 442001 Contact- 07152 -295502 Website : gsccwardha.
+          ac.in Email Id -gscc_wardha@rediffmail.com
+        </div>
+      </div>
+
+      <div>
+        <h1>Enquiries</h1>
+        <h2>Fill in the form below</h2>
+        <form action="#">
+          <input type="text" placeholder="Full name"></input>
+          <input type="text" placeholder="Your phone number"></input>
+          <input type="text" placeholder="Your email"></input>
+          <input type="text" placeholder="Enquiry"></input>
+          <input type="text" placeholder="Enquiry"></input>
+          <button> Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
